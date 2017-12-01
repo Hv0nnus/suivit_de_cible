26a54b6737e06beee418ed900842a69b5f49760d
Hv0nnus <tanguy.kerdoncuff@laposte.net>
2017-12-01 18:40:18 +0100

    the filter seems to work, but many strange things.

--- Filter for video/Kalman_New_Dimension.m
+++ Filter for video/Kalman_New_Dimension.m
@@ -1,6 +1,6 @@
 % This function will apply Kalman filter in 3D space
 % vecteur_x should not be here but we will use it to plot
-function [x_kalm_mean,x_kalm] = Kalman_New_Dimension(M,H,T,F,MQ,Q,R,vecteur_x_real,vecteur_y_disparity,variance_initial,n_particule,f_d, b,dPP)
+function [x_kalm_real] = Kalman_New_Dimension(M,H,T,F,MQ,Q,R,vecteur_x_real,vecteur_y_disparity,variance_initial,n_particule,f_d, b,dPP)
   
   n = 3;% length(x_init); % Dimension of the disparity space
 
@@ -8,44 +8,52 @@ function [x_kalm_mean,x_kalm] = Kalman_New_Dimension(M,H,T,F,MQ,Q,R,vecteur_x_re
   particule_disparity = ones(n+1,n_particule,T); %We will store the value of particule in disparity space here
   particule_real = ones(n+1,n_particule,T); %We will store the value of particule in disparity space here
 
-  U = randn(3,n_particule) * sqrtm(eye(n_particule)*variance_initial) + repmat(vecteur_y_disparity(:,1),1,n_particule); % start of different particule
-  particule_disparity(1:3,:,1) = U; % put of start value to the vector that will store the position
-  particule_real(:,:,1) = disparity_to_real(particule_disparity(:,:,1), f_d, b,dPP);
+  U = (randn(n_particule,3) * sqrtm(eye(3)*variance_initial))' + repmat(vecteur_y_disparity(1:3,1),1,n_particule); % start of different particule
+  particule_disparity(1:3,:,1) = U;
   
-  figure(1)
-  plot3(vecteur_y_disparity(1,:), vecteur_y_disparity(2,:), vecteur_y_disparity(3,:),'g')
-  hold on
-  plot3(particule_disparity(1,:,1),particule_disparity(2,:,1),particule_disparity(3,:,1),'r+')
 
-  figure(2)
-  plot3(particule_real(1,:,1), particule_real(2,:,1), particule_real(3,:,1),'r+')
-  hold on
-  plot3(vecteur_x_real(1,:),vecteur_x_real(2,:),vecteur_x_real(3,:),'b')
-  hold on
-
-
+  x_kalm_real(:,1) = disparity_to_real(vecteur_y_disparity(1:4,1), f_d, b,dPP);
   
-  %%P_kalm_disparity = ones(n,n,M); % Variance of the problem
-  %%%I don't know how to make the next loop faster...
-  %%for i = 1:M
-  %%  P_kalm_disparity(:,:,i) = eye(n,n);
-  %%end
-
-  %%x_kalm_disparity_mean = ones(n,T); % store the real value
-  %%x_kalm_real_mean = ones(n,T); % store the real value
+  x_kalm_disparity = ones(4,T);
+  x_kalm_disparity(:,1) = vecteur_y_disparity(1:4,1);
 
   for k=1:T-1
-    particule_real(:,:,k) = Markov_Kernel(F,Q,particule_real(:,:,k),n_particule);
-    plot3(particule_real(1,:,k), particule_real(2,:,k), particule_real(3,:,k),'g+');
-break
-break
-    samples_real = disparity_to_real(samples_disparity, f_d, b,dPP);
-    samples_real_updated = transition_real(samples_real,samples_speed);
-    samples_disparity_updated = real_to_disparity(samples_real_updated, f_d, b,dPP);
-    [x_kalm_disparity(:,:,k+1) P_kalm_disparity ]= recover_gaussian(samples_disparity_updated,M);
-    [x_kalm_disparity(:,:,k+1) P_kalm_disparity weight] = filtre_de_kalman_update( F, Q, H, R, vecteur_y_disparity(:,k),x_kalm_disparity(:,:,k), P_kalm_disparity,M,weight);
+    particule_real(:,:,k) = disparity_to_real(particule_disparity(:,:,k), f_d, b,dPP);
+    particule_real(:,:,k+1) = Markov_Kernel(F,Q,particule_real(:,:,k),n_particule);
+
+    particule_disparity(:,:,k+1) = real_to_disparity(particule_real(:,:,k+1), f_d, b,dPP);
     
-    x_kalm_disparity_mean(:,k+1) = weight*x_kalm_disparity(:,:,k+1)';
-    x_kalm_real_mean(:,k+1) = disparity_to_real(x_kalm_disparity_mean(:,k+1), f_d, b,dPP);
+    [x_kalm_disparity(1:3,k+1) P_kalm_disparity ]= recover_gaussian(particule_disparity(1:3,:,k+1));
+    [x_kalm_disparity(1:3,k+1) P_kalm_disparity ] = filtre_de_kalman_update(H, R, vecteur_y_disparity(1:3,k+1),x_kalm_disparity(1:3,k+1), P_kalm_disparity);
+    
+    x_kalm_real(:,k+1) = disparity_to_real(x_kalm_disparity(1:4,k+1), f_d, b,dPP);
+    particule_disparity(1:3,:,k+1) = (randn(n_particule,3) * sqrtm(P_kalm_disparity))' + repmat(x_kalm_disparity(1:3,k+1),1,n_particule);
   end
+  %plot
+  vecteur_y_real = disparity_to_real(vecteur_y_disparity, f_d, b,dPP);
+  figure(1)
+  plot3(vecteur_x_real(1,:), vecteur_x_real(2,:), vecteur_x_real(3,:),'b')
+  hold on
+  %plot3(vecteur_y_real(1,:),vecteur_y_real(2,:),vecteur_y_real(3,:),'g')
+  hold on
+  plot3(x_kalm_real(1,:),x_kalm_real(2,:),x_kalm_real(3,:),'r')
+  hold on
+  for k=1:T
+    plot3(particule_real(1,:,k), particule_real(2,:,k), particule_real(3,:,k),'r+');
+    hold on
+  end
+  
+  vecteur_x_disparity = real_to_disparity(vecteur_x_real, f_d, b,dPP);
+  figure(2)
+  plot3(vecteur_x_disparity(1,:), vecteur_x_disparity(2,:), vecteur_x_disparity(3,:),'b')
+  hold on
+  plot3(vecteur_y_disparity(1,:),vecteur_y_disparity(2,:),vecteur_y_disparity(3,:),'g')
+  hold on
+  plot3(x_kalm_disparity(1,:),x_kalm_disparity(2,:),x_kalm_disparity(3,:),'r')
+  hold on
+  for k=1:T
+    plot3(particule_disparity(1,:,k), particule_disparity(2,:,k), particule_disparity(3,:,k),'r+');
+    hold on
+  end
+
 end
--- Filter for video/principal3D.m
+++ Filter for video/principal3D.m
@@ -14,16 +14,16 @@ pkg load statistics
 fps = 1;
 
 % Tps temps d'enregistrement en seconde
-Tps = 10;
+Tps = 100;
 
 
 M = 1; % Number of gaussian that we use
 T_e = 1/fps;
 T = fps*Tps; %Number of observations 
 sigma_Q = 1;
-sigma_px = sigma_Q*0.05;
-sigma_py = sigma_Q*0.05;
-sigma_pz = sigma_Q*0.05;
+sigma_px = 50;
+sigma_py = 50;
+sigma_pz = 50;
 
 F_generate_trajectory = [ 1 T_e 0 0 0 0;
     0 1   0 0 0 0;
@@ -48,6 +48,9 @@ H = [ 1 0 0 0 0 0;
    0 0 1 0 0 0;
    0 0 0 0 1 0];
 
+%3D
+H = eye(3);
+   
 % H alternatif
 % H = [ 1 0 0 0 0 0;
 %    0 0 1 0 0 0;
@@ -80,6 +83,7 @@ R = [ sigma_px^2 0          0;
     
 % focale f
 f_d = [8/(4.4e-3*1624/800); 8/(4.4e-3*1224/600)];
+%f_d = [-8; -8];
 % Baseline
 b = 20;
 % Number of particule
@@ -99,7 +103,7 @@ x_init = [3 2 -4 4 5 6]';
 % x_init = [3 -4 2 40 20 30] ;
 
 
-variance_initial = 20000;
+variance_initial = 2000;
 
 distance_entre_camera = 10;
 position_camera_1 = [0,0,0];
@@ -113,19 +117,16 @@ vecteur_x_disparity = real_to_disparity(vecteur_x_modify, f_d, b,dPP);
 
 %vecteur_x_disparity = vecteur_x_disparity(1:3,:)/vecteur_x_disparity(4,:);
 
-vecteur_y_disparity = creat_observations_3D(H,R,vecteur_x_disparity(1:3,:),T);
+vecteur_y_disparity(1:3,:) = creat_observations_3D(H,R,vecteur_x_disparity(1:3,:),T);
+vecteur_y_disparity(4,:) = ones(1,T);
 %vecteur_y_disparity = vecteur_x_disparity(1:3,:) %Try with real value
-vecteur_y_disparity
-vecteur_x_disparity
-real_to_disparity(disparity_to_real(vecteur_x_disparity, f_d, b,dPP), f_d, b,dPP)
-[x_kalm_mean,x_kalm] = Kalman_New_Dimension(M,H,T,F,MQ,Q,R,vecteur_x,vecteur_y_disparity,variance_initial,n_particule,f_d, b,dPP)
-
-x_kalm_mean_real = x_kalm_mean([1 3 5],:);
-x_kalm_mean_real(4,:) = ones(1,T);
-[ x_kalm_mean_real ] = disparity_to_real(x_kalm_mean_real, f_d, b,dPP);
 
 
-[eq , eqm] = mean_erreur_quadratique_suj(vecteur_x([1 3 5],:), x_kalm_mean_real(1:3,:), T );
+[x_kalm_real] = Kalman_New_Dimension(M,H,T,F,MQ,Q,R,vecteur_x_modify,vecteur_y_disparity,variance_initial,n_particule,f_d, b,dPP);
+
+
+
+[eq , eqm] = mean_erreur_quadratique_suj(vecteur_x([1 3 5],:), x_kalm_real(1:3,:), T );
 % The eqm is very high it should be a vecteur computing the error for each
 % component of the vector or at least an eqm for the position vector and
 % one for the speed vector
@@ -134,10 +135,10 @@ eqm
 %eqm
 
 % Trajectoires dans disparity
-figure(1)
-plot3(vecteur_x_disparity(1,:), vecteur_x_disparity(2,:), vecteur_x_disparity(3,:),'b')
+%figure(3)
+%plot3(vecteur_x_disparity(1,:), vecteur_x_disparity(2,:), vecteur_x_disparity(3,:),'b')
 hold on
-plot3(vecteur_y_disparity(1,:), vecteur_y_disparity(2,:), vecteur_y_disparity(3,:),'g')
+%plot3(vecteur_y_disparity(1,:), vecteur_y_disparity(2,:), vecteur_y_disparity(3,:),'g')
 hold on
 %plot3(x_kalm_mean(1,:), x_kalm_mean(3,:),x_kalm_mean(5,:),'*')
 hold on
@@ -146,10 +147,10 @@ for i=1:M
 end
 
 % Trajectoires dans real
-figure(2)
-plot3(vecteur_x(1,:), vecteur_x(3,:), vecteur_x(5,:),'b')
-%hold on
-%plot3(vecteur_y_disparity(1,:), vecteur_y_disparity(2,:), vecteur_y_disparity(3,:),'g')
+%figure(4)
+%plot3(vecteur_x(1,:), vecteur_x(3,:), vecteur_x(5,:),'b')
+hold on
+%plot3(x_kalm_real(1,:), x_kalm_real(2,:), x_kalm_real(3,:),'r')
 hold on
 %plot3(x_kalm_mean_real(1,:), x_kalm_mean_real(2,:),x_kalm_mean_real(3,:),'*')
 hold on
